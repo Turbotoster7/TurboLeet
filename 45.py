50e9082331ddc36667d68c7cc9d6d9c5f70de51d
Bartek <dwajper@gmail.com>
2026-06-21 20:26:27 +0200

    solved 45 leet

--- 45.py
+++ 45.py
@@ -1,3 +1,41 @@
+from collections import deque
+
+
+
 class Solution:
-    def jump(self, nums: List[int]) -> int:
+    def jump(self, nums) -> int:
+        # if len(nums) == 1:
+        #     return 0
+        # q = deque()
+        # q.append(0)
+        # visited = {}
+        # visited[0] = 0
+        # while q:
+        #     number = q.popleft()
+        #     for n in range(1, nums[number] + 1):
+        #         next_idx = number + n
+        #         if next_idx >= len(nums):
+        #             break
+        #         if next_idx == len(nums) - 1:
+        #             return visited[number] + 1
+        #         if next_idx not in visited:
+        #             visited[next_idx] = visited[number] + 1
+        #             q.append(next_idx)
+
+        # return visited[len(nums) - 1]
         
+        if len(nums) == 1:
+            return 0
+        jumps = 0
+        current_end = 0
+        farthest = 0
+        for n in range(len(nums) - 1):
+            farthest = max(farthest, n + nums[n])
+            if n == current_end:
+                current_end = farthest
+                jumps += 1
+        return jumps
+
+s = Solution()
+nums = [2,3,1,1,4]
+print(s.jump(nums))
